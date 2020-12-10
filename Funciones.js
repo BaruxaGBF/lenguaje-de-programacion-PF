bfcc8b81011e3b916ed789eb32e910d5c0828666
wilkar marquez <wilkarmarquez13@gmail.com>
2020-12-10 18:12:15 -0500

    anexando botones

--- Funciones.js
+++ Funciones.js
@@ -1,17 +1,3 @@
-$(function(){
-    var textArea = $('#inputprincipal');
-    var hiddenDiv = $(document.createElement('div'));
-    var inputprincipal = null;
-    
-    textArea.addClass('noscroll');
-    hiddenDiv.addClass('hiddendiv');
-    
-    $(textArea).after(hiddenDiv);
-    
-    textArea.on('keyup', function(){
-        inputprincipal = $(this).val();
-        inputprincipal = inputprincipal.replace(/n/g, '<br>');
-        hiddenDiv.html(inputprincipal + '<br class="line">');
-        $(this).css('height', hiddenDiv.height());
-    });
-});
+function documentacion(){
+    location.href ="Documentacion.html"
+}
